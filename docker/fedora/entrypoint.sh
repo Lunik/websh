e8741e78b43e4563b1407c35121fde0413a00316
lunik <guillaume.lunik@gmail.com>
2018-06-02 13:44:37 +0200

    add new distro

---
+++ docker/fedora/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+screenfetch -L > /etc/motd
+echo -e "\n       Welcome to Fedora\n" >> /etc/motd
+
+if [ -z "$USERNAME" ]; then
+  USERNAME="demo"
+fi
+
+useradd \
+  -s /bin/bash \
+  -m \
+  -d /home/$USERNAME \
+  $USERNAME
+
+echo "$USERNAME:$USERNAME" | chpasswd
+
+echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+exec "$@"
